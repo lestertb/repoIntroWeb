fa1035f2b107891df61d86d8102c59de0764ded2
david_1797 <da.arce.97@gmail.com>
2022-01-04 18:25:30 -0600

    whiteboard and notes

--- js/workflows.js
+++ js/workflows.js
@@ -1,6 +1,10 @@
-dragElement(document.getElementById("mydiv"));
+//dragElement(document.getElementById("mydiv"));
 
-function dragElement(elmnt) {
+function dragElement(elmnt1) {
+
+  console.log(elmnt1);
+
+  elmnt = document.getElementById(elmnt1);
 
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
@@ -40,4 +44,43 @@ function dragElement(elmnt) {
     document.onmouseup = null;
     document.onmousemove = null;
   }
+
+}
+
+var contadorNotas = 1;
+
+function addNote(textNote){
+  const newNote = document.createElement("div");
+  const text = document.createTextNode(document.getElementById(textNote).value);
+
+  console.log(text);
+
+  if (text.textContent === ""){
+    alert("Las notas no pueden estar en blanco");
+    return
+  }
+
+  var color = Math.floor(Math.random() * (4 - 1)) + 1;
+  console.log(color);
+
+  if (color === 1){
+    newNote.setAttribute("style","top: 429px; left: 534px; background: linear-gradient(to bottom, #e7da8f 20% , #faec9b 20%);");
+  }else if (color === 2) {
+    newNote.setAttribute("style","top: 429px; left: 534px; background: linear-gradient(to bottom, #739ebb 20% , #8ec2e4 20%);");
+  }else if (color === 3){
+    newNote.setAttribute("style","top: 429px; left: 534px; background: linear-gradient(to bottom, #cea680 20% , #e4b88e 20%);");
+  }else{
+
+  }
+
+  newNote.setAttribute("id",contadorNotas);
+  newNote.setAttribute("class","note");
+  newNote.setAttribute("onclick","dragElement(id)");
+  //newNote.setAttribute("style","top: 429px; left: 534px; background: #" + randomColor);
+  
+  document.body.appendChild(newNote)
+  newNote.appendChild(text)
+
+  contadorNotas = contadorNotas + 1;
+
 }
